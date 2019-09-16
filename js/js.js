9a03e26c1fb88b2e041b4b8fb3cc1e87a954f421
Jeppe Lund <sejersenlund@hotmail.dk>
2019-09-16 12:42:40 +0200

    Rettet JS rækkefølge

--- js/js.js
+++ js/js.js
@@ -1,5 +1,20 @@
 // JavaScript Document
 
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#cloud1').css('top',(0-(scrolled*.25))+'px');
+    $('#cloud2').css('top',(0-(scrolled*.5))+'px');
+    $('#cloud3').css('top',(0-(scrolled*.75))+'px');
+	$('#cloud4').css('top',(0-(scrolled*.25))+'px');
+    $('#cloud5').css('top',(0-(scrolled*.5))+'px');
+    $('#cloud6').css('top',(0-(scrolled*.75))+'px');
+	$('#cloud7').css('top',(0-(scrolled*.75))+'px');
+}
+
 var slideIndex = [1,1];
 var slideId = ["slides"]
 showSlides(1, 0);
@@ -20,18 +35,3 @@ function showSlides(n, no) {
   x[slideIndex[no]-1].style.display = "block";  
 }
 
-
-$(window).bind('scroll',function(e){
-    parallaxScroll();
-});
-
-function parallaxScroll(){
-    var scrolled = $(window).scrollTop();
-    $('#cloud1').css('top',(0-(scrolled*.25))+'px');
-    $('#cloud2').css('top',(0-(scrolled*.5))+'px');
-    $('#cloud3').css('top',(0-(scrolled*.75))+'px');
-	$('#cloud4').css('top',(0-(scrolled*.25))+'px');
-    $('#cloud5').css('top',(0-(scrolled*.5))+'px');
-    $('#cloud6').css('top',(0-(scrolled*.75))+'px');
-	$('#cloud7').css('top',(0-(scrolled*.75))+'px');
-}
